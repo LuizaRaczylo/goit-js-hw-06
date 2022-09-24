a369dd7da8546c56b623197b2152b75a2c738c37
Luiza Raczyło <luiza.raczylo@gmail.com>
2022-09-24 23:19:48 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+//Napisz skrypt, który dla każdego elementu tablicy ingredients:
+
+// Utworzy oddzielny element <li>. Koniecznie użyj metody document.createElement().
+// Doda nazwę elementu jako jego zawartość tekstową.
+// Doda do elementu klasę item.
+// Po czym umieści wszystkie <li> na liście podczas jednej operacji ul#ingredients.
+
+const list = document.querySelector("#ingredients");
+
+const newList = ingredients.map((item) => {
+  const newElement = document.createElement("li");
+  newElement.textContent = item;
+  newElement.classList.add("item");
+  return newElement;
+});
+
+list.append(...newList);
