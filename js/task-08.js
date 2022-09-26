a2e15629d695db25f78cdaa8350e7953deffa58c
Luiza Raczyło <luiza.raczylo@gmail.com>
2022-09-27 00:26:19 +0200

    commit task 08 & 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+// Opracowanie przesyłania formularza form.login-form powinno przebiegać zgodnie ze zdarzeniem submit.
+// Podczas przesyłania formularza strona nie powinna się restartować.
+// Jeśli w formularzu są nieuzupełnione pola, wprowadź alert z upomnieniem o tym, że wszystkie pola powinny zostać wypełnione.
+// Jeśli użytkownik uzupełnił wszystkie pola i wysłał formularz, zbierz wartość pól jako obiekt, gdzie nazwa pola będzie nazwą właściwości, a wartość pola - wartością właściwości. Aby otrzymać dostęp do elementów formularza użyj właściwości elements.
+// Umieść obiekt z wprowadzonymi danymi do wiersza poleceń i wyczyść wartości pól formularza metodą reset.
 
+const newForm = document.querySelector(".login-form");
+
+const newSubmit = (event) => {
+    event.preventDefault();
+
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all fields");
+    }
+    else {
+        const result = { email: email.value, password: password.value };
+        console.log(result);
+    }
+    event.currentTarget.reset();
+};
+
+newForm.addEventListener("submit", newSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// Napisz skrypt, który zmienia kolor tła elementu < body > poprzez style inline po kliknięciu na button.change - color 
+// i wprowadza wartość koloru do span.color.
+// Aby wygenerować losowy kolor użyj funkcji getRandomHexColor.
+
+const newBody = document.querySelector("body");
+const colorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+const randomColor = ()=> {
+  colorSpan.textContent = newBody.style.backgroundColor=getRandomHexColor();
+}
+colorButton.addEventListener("click", randomColor);
